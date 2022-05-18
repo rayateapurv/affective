1ddf89d830871aac28a2b1d985ab4b1c2d77b45b
Apurv Rayate <rayateapurv@gmail.com>
2022-05-19 05:16:38 +0530

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+let playbool = "false";
+//console.log(speech);
+
+document.querySelector("#speech1").addEventListener("click", () => {
+  speech.text = document.querySelector("#speech1content").innerHTML;
+  speech.rate = 7;
+  //console.log(window.speechSynthesis.getVoices());
+  playbool = !playbool;
+  //console.log(playbool);
+});
+
+setInterval(() => {
+  if (playbool) {
+    //console.log(speech);
+    window.speechSynthesis.speak(speech);
+  } else if (!playbool) {
+    window.speechSynthesis.cancel();
+  }
+}, 1000);
